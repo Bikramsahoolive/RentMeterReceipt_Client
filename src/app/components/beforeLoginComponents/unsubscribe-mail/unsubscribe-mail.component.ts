9aff1a155475c951c5d7f5aa129c1aff20b1986f
Bikram Sahoo <bikramsahoo@live.in>
2024-07-12 02:49:18 +0530

    missing unsubscrib file.

---
+++ src/app/components/beforeLoginComponents/unsubscribe-mail/unsubscribe-mail.component.ts
@@ -0,0 +1,75 @@
+import { Component,ViewChild,ElementRef } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { SignupService } from 'src/app/services/signupService/signup.service';
+
+@Component({
+  selector: 'app-unsubscribe-mail',
+  templateUrl: './unsubscribe-mail.component.html',
+  styleUrls: ['./unsubscribe-mail.component.css']
+})
+export class UnsubscribeMailComponent {
+  constructor(private route:ActivatedRoute,private router:Router,private spinner:NgxSpinnerService,private toaster :ToastrService,private signupServe:SignupService){}
+  @ViewChild('emailInput',{ static: false })email!:ElementRef;
+  emailid:string='';
+  ngAfterViewInit() {
+    let urlId = this.route.snapshot.paramMap.get('id');
+    let emailId;
+    
+    if(urlId && urlId!==null){
+      emailId = atob(urlId);
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if(!emailRegex.test(emailId)){
+        this.toaster.error('Invalid Request',"",{positionClass:'toast-top-center',progressBar:true});
+        this.router.navigate(['']);
+      }
+      this.email.nativeElement.value=emailId; 
+      this.emailid = emailId;
+    }else{
+      this.email.nativeElement.removeAttribute('readonly');
+    }
+  }
+
+
+  unsubscribe(){
+    
+    let data={
+      email:""
+    };
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+
+    if(this.emailid!==""){
+      data.email = this.emailid;
+    }else{
+      if(this.email.nativeElement.value===""|| !emailRegex.test(this.email.nativeElement.value)){
+        this.toaster.info('Invalid Email Input.',"",{positionClass:'toast-top-center',progressBar:true});
+        return;
+      }
+
+      data.email = this.email.nativeElement.value;
+    }
+    this.spinner.show();
+    this.signupServe.sendUnsubMail(data).subscribe({
+      next:(res:any)=>{
+        if(res.status==='success'){
+        this.toaster.success(res.message,"",{positionClass:'toast-top-center',progressBar:true});
+        this.router.navigate(['']);
+        }else{
+          this.toaster.error(res.message,"",{positionClass:'toast-top-center',progressBar:true});
+        }
+        this.spinner.hide();
+      },error:(err)=>{
+        this.spinner.hide();
+        console.log(err.error);
+        this.toaster.error('Something wents wrong.',"",{positionClass:'toast-top-center',progressBar:true});
+      }
+    });
+    
+    
+  }
+
+
+}
