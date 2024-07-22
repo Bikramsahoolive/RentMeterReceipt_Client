fab9251069a7b3899df2e81f8e2aed09729b67d9
Bikram Sahoo <bikramsahoo@live.in>
2024-07-22 09:36:09 +0530

    correct the production url

--- src/environment.ts
+++ src/environment.ts
@@ -1,8 +1,8 @@
 
 
 export const environment = {
-    apiUrl: 'https://c8bltjmv-3000.inc1.devtunnels.ms',
-    // apiUrl:'https://rnmrs.onrender.com',
+    // apiUrl: 'https://c8bltjmv-3000.inc1.devtunnels.ms',
+    apiUrl:'https://rnmrs.onrender.com',
      siteKey : "6LdR0QMqAAAAAO9bw2BnT9RxX6tn1C7NfXqdzgxP",
      razorpay_ket:"rzp_test_GkaVaKzCP6Vpxg"
   };
