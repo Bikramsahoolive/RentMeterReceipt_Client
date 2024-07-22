6b09b4d231e8dcb9babda3aba2cf6f49f8b7d6e2
Bikram Sahoo <bikramsahoo@live.in>
2024-07-23 00:14:41 +0530

    build total due table for landlord.

--- src/app/components/landlord-components/total-due/total-due.component.ts
+++ src/app/components/landlord-components/total-due/total-due.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { create } from 'qrcode';
+import { rentBillData, rentholderData } from 'src/app/model/data';
+import { LandlordService } from 'src/app/services/landlordService/landlord.service';
 
 @Component({
   selector: 'app-total-due',
@@ -6,5 +11,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./total-due.component.css']
 })
 export class TotalDueComponent {
+  constructor(private landloreServe : LandlordService, private spinner:NgxSpinnerService, private toastr:ToastrService){}
+  rentholderData:any=[{
+    id:"",
+    name:"",
+    paid_amt:0,
+    bill_amt:0
+  }]
 
+  ngOnInit(){
+    
+    let finalArray:any = [];
+    this.spinner.show();
+      this.landloreServe.getAllRentholder().subscribe({
+        next:(res:rentholderData[])=>{
+          // this.rentholderData = res;
+          res.forEach((e:any)=>{
+            let obj = {id:e.id,name:e.name,paid_amt:e.paid_amt};
+            finalArray.push(obj);
+          });
+
+          this.landloreServe.getAllRentBillData().subscribe({
+        next:(res:rentBillData[])=>{
+
+            finalArray.forEach((obj:any)=>{
+              let billAmt = 0;
+              res.forEach((bill:any)=>{
+                if(obj.id === bill.rentholder_id){
+                  billAmt += bill.final_amt;
+                }
+              });
+              obj.bill_amt = billAmt;
+            });
+            this.rentholderData = finalArray;
+            this.spinner.hide();
+        },
+        error:(err)=>{
+          this.spinner.hide();
+          console.log(err.error);
+          if(!err.error.status){
+            this.toastr.info(err.error.message,'',{progressBar:true,positionClass:"toast-top-center"});
+          }else{
+            this.toastr.error('something wents wrong.','Error',{progressBar:true,positionClass:"toast-top-center"});
+          }
+        }
+      });
+          
+          this.spinner.hide();
+        },error:(err)=>{
+          this.toastr.error('something wents wrong',"Error",{positionClass:'toast-top-center',progressBar:true});
+          console.log(err.error);
+          this.spinner.hide();
+          
+        }
+      });
+
+  }
 }
