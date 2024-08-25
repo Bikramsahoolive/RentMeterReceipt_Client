f17c95ffcfbc9d779d40002d20c6e7794996a484
Bikram Sahoo <bikramsahoo@live.in>
2024-08-25 22:36:00 +0530

    change favicon, icon and install prompt for new user

--- src/app/components/beforeLoginComponents/header/header.component.ts
+++ src/app/components/beforeLoginComponents/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthServiceService } from 'src/app/services/auth Service/auth-service.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-header',
@@ -8,9 +9,47 @@ import { AuthServiceService } from 'src/app/services/auth Service/auth-service.s
 })
 export class HeaderComponent {
   constructor(public authService:AuthServiceService){}
+
+  deferredPrompt:any;
+  showPopup:boolean=false;
+
   ngOnInit(){
     localStorage.setItem('connect.rid',btoa('false'));
       this.authService.setHeader();
+
+      window.addEventListener('beforeinstallprompt',(event:any)=>{
+        event.preventDefault();
+        this.deferredPrompt = event;
+        const currentTime = String(Date.now());
+        const setTime = localStorage.getItem('app-prompt')||'';
+        if(currentTime >= setTime){
+        Swal.fire({
+          html:` <span> Get best experience! Install RentⓝMeter.Receipt for offline use and enjoy faster access. <br>
+          <strong>Install App Now !</strong>
+          </span>  `,
+          showCloseButton:true,
+          confirmButtonText:"Install",
+          position:'top'
+        })
+        .then((res)=>{
+          if(res.isConfirmed){
+            this.deferredPrompt.prompt();
+            localStorage.removeItem('app-prompt');
+            this.deferredPrompt.userChoice.then((choiceResult:any)=>{
+              if(choiceResult.outcome ==='accepted'){
+                console.log("app installed");
+              }else{
+            const timeer = String(Date.now()+86400000);
+            localStorage.setItem('app-prompt',timeer);
+              }
+            })
+          }else{
+            const timeer = String(Date.now()+86400000);
+            localStorage.setItem('app-prompt',timeer);
+          }
+        })
+      }
+  })
  }
   dropdownToggle(dropdown:any,tglBtn:any)
   {
